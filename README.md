e76db456e0e52a9a5ee77e3291e1a4710064c958
Nikola Stamatovic <stamatmail@gmail.com>
2019-08-08 20:45:05 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# jquery.youtube-background
+
+Another 100 liner in a form of jQuery plugin created to facilitate YouTube embeds as a cover background using YouTube Embed API.
+
+I wrote this code several times over the years and never bothered to make it reusable. Now when I needed it again I could not even find where I wrote it last...
+
+Goodbye careless days... I'm getting old...
+
+[DEMO HERE ⤻](http://stamat.github.io/jquery.youtube-background)
+
+## Usage
+
+Usage is pretty simple, add a data attribute **data-youtube** containing a full YouTube link or just the YouTube ID.
+
+You can trigger all elements containing the noted attribute with `$("[data-youtube]").expandable();`, or specify your selector, on jQuery document ready event.
+
+P.S. *https://www.youtube.com/player_api* is injected automatically, only once per script init. I've seen some implementations like Elementor WP plugin that inject it several times, for no reason. Anyway, you're welcome.
+
+#### Example
+
+```
+    <div id="ytbg" data-youtube="https://www.youtube.com/watch?v=eEpEeyqGlxA"></div>
+    
+    <script type="text/javascript">
+        jQuery(document).ready(function() {
+            $('[data-youtube]').youtube_background();
+        });
+    </script>
+```
+
+THE END.
---
+++ jquery.youtube-background.js
@@ -0,0 +1,106 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/player_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+(function ($) {
+
+    $.fn.youtube_background = function() {
+        var YOUTUBE = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
+
+        var $this = $(this);
+
+        function onVideoPlayerReady(event) {
+            event.target.playVideo();
+
+            $(event.target.a).css({
+                "top" : "50%",
+                "left" : "50%",
+                "transform": "translateX(-50%) translateY(-50%)",
+                "position": "absolute"
+            });
+
+            var $root = $(event.target.a).parent();
+
+            function onResize() {
+                var h = $root.outerHeight() + 100; // since showinfo is deprecated and ignored after September 25, 2018. we add +100
+                var w = $root.outerWidth() + 100;
+                var res = 1.77777778;
+
+                if (res > w/h) {
+                    $root.find('iframe').width(h*res).height(h);
+                } else {
+                    $root.find('iframe').width(w).height(w/res);
+                }
+            }
+            $(window).on('resize', onResize);
+            onResize();
+        }
+
+        function onVideoStateChange(event) {
+            event.target.playVideo();
+        }
+
+        var ytp = null;
+        var yt_event_triggered = false;
+
+        window.onYouTubeIframeAPIReady = function () {
+            yt_event_triggered = true;
+
+             //element loop
+            for (var i = 0; i < $this.length; i++) {
+                var $elem = $($this[i]);
+
+                if ($elem.parent().hasClass('youtube-background')) {
+                    continue;
+                }
+
+                $elem.wrap('<div class="youtube-background" />');
+                var $root = $elem.parent();
+
+                $root.css({
+                    "height" : "100%",
+                    "width" : "100%",
+                    "z-index": "0",
+                    "position": "absolute",
+                    "overflow": "hidden"
+                });
+
+                $root.parent().parent().css({
+                    "position": "relative"
+                });
+
+                var ytid = $elem.data('youtube');
+
+                var pts = ytid.match(YOUTUBE);
+                if (pts && pts.length) {
+                    ytid = pts[1];
+                }
+
+                ytp = new YT.Player($elem[0], {
+                    height: '1080',
+                    width: '1920',
+                    videoId: ytid,
+                    playerVars: {
+                        'controls': 0,
+                        'autoplay': 1,
+                        'mute': 1,
+                        'loop': 1,
+                        'showinfo': 0,
+                        'modestbranding': 1
+                    },
+                    events: {
+                        'onReady': onVideoPlayerReady,
+                        'onStateChange': onVideoStateChange
+                    }
+                });
+            }
+        };
+
+        if (window.hasOwnProperty('YT') && window.YT.loaded && !yt_event_triggered) {
+            window.onYouTubeIframeAPIReady();
+        }
+
+ 		return $this;
+ 	};
+})(jQuery);
