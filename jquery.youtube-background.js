56ee38424d0fabcd1598cfe4ae62c1f6bcf0c366
Nikola Stamatovic <stamatmail@gmail.com>
2021-11-11 22:28:12 +0100

    build --production

--- jquery.youtube-background.js
+++ jquery.youtube-background.js
@@ -426,45 +426,316 @@
     this.controls_element.appendChild(btn);
   };
 
-  function VideoBackgrounds(selector, params) {
-    this.elements = selector;
+  function VimeoBackground(elem, params, id, uid) {
+  	this.is_mobile = isMobile();
 
-    if (typeof selector === 'string') {
-      this.elements = document.querySelectorAll(selector);
+  	this.element = elem;
+  	this.ytid = id;
+  	this.uid = uid;
+  	this.player = null;
+  	this.buttons = {};
+
+  	this.state = {};
+  	this.state.play = false;
+  	this.state.mute = false;
+
+  	this.params = {};
+
+  	this.defaults = {
+  		'pause': false, //deprecated
+  		'play-button': false,
+  		'mute-button': false,
+  		'autoplay': true,
+  		'muted': true,
+  		'loop': true,
+  		'mobile': false,
+  		'load-background': true,
+  		'resolution': '16:9',
+      'inline-styles': true,
+      'fit-box': false,
+  		'offset': 200
+  	};
+
+  	this.__init__ = function () {
+  		if (!this.ytid) {
+  			return;
+  		}
+
+  		this.parseProperties(params);
+  		this.params.resolution_mod = parseResolutionString(this.params.resolution);
+  		this.state.playing = this.params.autoplay;
+  		this.state.muted = this.params.muted;
+
+  		this.buildHTML();
+  		this.injectIFrame();
+  	};
+
+  	this.__init__();
+  }
+
+  VimeoBackground.prototype.parseProperties = function (params) {
+  	if (!params) {
+  		this.params = this.defaults;
+  	} else {
+  		//load in defaults
+  		for (var k in this.defaults) {
+  			if (!this.params.hasOwnProperty(k)) {
+  				this.params[k] = this.defaults[k];
+  			}
+  		}
+  	}
+
+  	// load params from data attributes
+  	for (var k in this.params) {
+  		var data = this.element.getAttribute('data-ytbg-'+k);
+
+  		if (data !== undefined && data !== null) {
+  			data = data === 'false' ? false : data;
+  			this.params[k] = data;
+  		}
+  	}
+
+  	//pause deprecated
+  	if (this.params.pause) {
+  		this.params['play-button'] = this.params.pause;
+  	}
+  };
+
+  VimeoBackground.prototype.injectIFrame = function () {
+  	this.iframe = document.createElement('iframe');
+  	this.iframe.setAttribute('frameborder', 0);
+  	this.iframe.setAttribute('allow', ['autoplay; mute']);
+  	var src = 'https://player.vimeo.com/video/'+this.ytid+'?background=1&controls=0';
+
+  	if (this.params.muted) {
+  		src += '&muted=1';
+  	}
+
+  	if (this.params.autoplay) {
+  		src += '&autoplay=1';
+  	}
+
+  	if (this.params.loop) {
+  		src += '&loop=1&autopause=0';
+  	}
+
+  	this.iframe.src = src;
+
+  	if (this.uid) {
+  		this.iframe.id = this.uid;
+  	}
+
+    if (this.params['inline-styles']) {
+    	this.iframe.style.top = '50%';
+    	this.iframe.style.left = '50%';
+    	this.iframe.style.transform = 'translateX(-50%) translateY(-50%)';
+    	this.iframe.style.position = 'absolute';
+    	this.iframe.style.opacity = 1;
     }
 
-    this.index = {};
-    this.re = {};
-    this.re.YOUTUBE = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
+  	this.element.parentNode.appendChild(this.iframe);
+  	this.iframe.parentNode.removeChild(this.element);
+
+    if (this.params['fit-box']) {
+      this.iframe.style.width = '100%';
+      this.iframe.style.height = '100%';
+    } else {
+      var self = this;
+
+      function onResize() {
+        var h = self.iframe.parentNode.offsetHeight + self.params.offset; // since showinfo is deprecated and ignored after September 25, 2018. we add +200 to hide it in the overflow
+        var w = self.iframe.parentNode.offsetWidth + self.params.offset;
+        var res = self.params.resolution_mod;
+
+        if (res > w/h) {
+          self.iframe.style.width = h*res + 'px';
+          self.iframe.style.height = h + 'px';
+        } else {
+          self.iframe.style.width = w + 'px';
+          self.iframe.style.height = w/res + 'px';
+        }
+      }
+
+      window.addEventListener('resize', onResize);
+      onResize();
+    }
+  };
+
+  VimeoBackground.prototype.buildHTML = function () {
+  	var parent = this.element.parentNode;
+  	// wrap
+  	var wrapper = document.createElement('div');
+  	wrapper.className = 'youtube-background';
+  	parent.insertBefore(wrapper, this.element);
+  	wrapper.appendChild(this.element);
+  	var id = this.element.id;
+  	this.element.id = '';
+  	wrapper.id = id;
+
+  	//set css rules
+  	var wrapper_styles = {
+  		"height" : "100%",
+  		"width" : "100%",
+  		"z-index": "0",
+  		"position": "absolute",
+  		"overflow": "hidden",
+  		"top": 0, // added by @insad
+  		"left": 0,
+  		"bottom": 0,
+  		"right": 0
+  	};
+
+  	if (!this.params['mute-button']) {
+  		wrapper_styles["pointer-events"] = "none"; // avoid right mouse click popup menu
+  	}
+
+  	if (this.params['load-background']) {
+  		//TODO: wrapper_styles['background-image'] = 'url(https://img.youtube.com/vi/'+this.ytid+'/maxresdefault.jpg)';
+  		wrapper_styles['background-size'] = 'cover';
+  		wrapper_styles['background-repeat'] = 'no-repeat';
+  		wrapper_styles['background-position'] = 'center';
+  	}
+
+    if (this.params['inline-styles']) {
+    	for (var property in wrapper_styles) {
+    		wrapper.style[property] = wrapper_styles[property];
+    	}
+
+      wrapper.parentNode.style.position = 'relative';
+    }
+
+  	if (this.is_mobile && !this.params.mobile) {
+  		return wrapper;
+  	}
+
+  	return wrapper;
+  };
+
+  function ActivityMonitor(on_activity, on_inactivity, activity_timeout, inactivity_timeout, events) {
+  	this.timer = null;
+  	this.timeout = inactivity_timeout || 10000;
+  	this.activity_timer = null; //for event throttling
+  	this.activity_timeout = activity_timeout || 1000;
+  	this.last_activity = null;
+
+  	this.resetTimer = function() {
+  		if (this.timer) {
+  			clearTimeout(this.timer);
+  			this.timer = null;
+  		}
+
+  		var self = this;
+  		this.timer = setTimeout(function() {
+  			if (self.last_activity + self.timeout + self.activity_timeout
+  				>= new Date().getTime()) {
+  				if (on_inactivity) {
+  					on_inactivity();
+  				}
+  			}
+  		}, this.timeout);
+  	};
+
+  	this.logActivity = function() {
+  		this.last_activity = new Date().getTime();
+
+  		if (on_activity) {
+  			on_activity();
+  		}
+  	};
+
+  	this.onActivity = function() {
+  		if (!this.activity_timer) {
+  			var self = this;
+  			this.activity_timer = setTimeout(function(){
+  				self.logActivity();
+  				self.resetTimer();
+
+  				clearTimeout(self.activity_timer);
+  				self.activity_timer = null;
+  			}, this.activity_timeout);
+  		}
+  	};
+
+  	this.__init__ = function() {
+  		var self = this;
+
+  		if (!events) {
+  			events = ['click', 'mousemove', 'scroll'];
+  		} else {
+  			if (typeof events === 'string') {
+  				events = [events];
+  			}
+  		}
+
+  		for (var i = 0; i < events.length; i++) {
+  			document.addEventListener(events[i], function() {
+  				self.onActivity();
+  			});
+  		}
+  	};
+
+  	this.__init__();
+  }
+
+  function VideoBackgrounds(selector, params) {
+  	this.elements = selector;
+
+  	if (typeof selector === 'string') {
+  		this.elements = document.querySelectorAll(selector);
+  	}
+
+  	this.index = {};
+  	this.re = {};
+  	this.re.YOUTUBE = /(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i;
     this.re.VIMEO = /(?:www\.|player\.)?vimeo.com\/(?:channels\/(?:\w+\/)?|groups\/(?:[^\/]*)\/videos\/|album\/(?:\d+)\/video\/|video\/|)(\d+)(?:[a-zA-Z0-9_\-]+)?/i;
 
-    this.__init__ = function () {
-      for (var i = 0; i < this.elements.length; i++) {
-        var element = this.elements[i];
+  	this.__init__ = function () {
+  		for (var i = 0; i < this.elements.length; i++) {
+  			var element = this.elements[i];
 
-        var link = element.getAttribute('data-youtube');
-        var vid_data = this.getVidID(link);
+  			var link = element.getAttribute('data-youtube');
+  			var vid_data = this.getVidID(link);
 
         if (!vid_data) {
           continue;
         }
 
-        var uid = this.generateUID(vid_data.id);
+  			var uid = this.generateUID(vid_data.id);
 
         if (!uid) {
-          continue;
-        }
+  				continue;
+  			}
 
         if (vid_data.type === 'YOUTUBE') {
           var yb = new YoutubeBackground(element, params, vid_data.id, uid);
           this.index[uid] = yb;
+        } else if (vid_data.type === 'VIMEO') {
+          var vm = new VimeoBackground(element, params, vid_data.id, uid);
+          this.index[uid] = vm;
         }
-      }
+  		}
 
-      this.initYTPlayers();
-    };
+  		var self = this;
 
-    this.__init__();
+  		this.initYTPlayers(function() {
+  			//TODO: FIX!
+  			if (params &&
+  				(params.hasOwnProperty('activity_timeout')
+  					|| params.hasOwnProperty('inactivity_timeout'))) {
+  				this.activity_monitor = new ActivityMonitor(function () {
+  						self.playVideos();
+  					}, function() {
+  						self.pauseVideos();
+  					},
+  					params ? params.activity_timeout : null,
+  					params ? params.inactivity_timeout : null,
+  					['mousemove', 'scroll']
+  				);
+  			}
+  		});
+  	};
+
+  	this.__init__();
   }
 
   VideoBackgrounds.prototype.getVidID = function (link) {
@@ -478,61 +749,61 @@
           return {
             id: pts[1],
             type: k
-          };
+          }
         }
       }
     }
 
-    return null;
+  	return null;
   };
 
 
   VideoBackgrounds.prototype.generateUID = function (pref) {
-    //index the instance
-    function getRandomIntInclusive(min, max) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
-    }
+  	//index the instance
+  	function getRandomIntInclusive(min, max) {
+  		min = Math.ceil(min);
+  		max = Math.floor(max);
+  		return Math.floor(Math.random() * (max - min + 1)) + min; //The maximum is inclusive and the minimum is inclusive
+  	}
 
-    var uid = pref +'-'+ getRandomIntInclusive(0, 9999);
-    while (this.index.hasOwnProperty(uid)) {
-      uid = pref +'-'+ getRandomIntInclusive(0, 9999);
-    }
+  	var uid = pref +'-'+ getRandomIntInclusive(0, 9999);
+  	while (this.index.hasOwnProperty(uid)) {
+  		uid = pref +'-'+ getRandomIntInclusive(0, 9999);
+  	}
 
-    return uid;
+  	return uid;
   };
 
   VideoBackgrounds.prototype.pauseVideos = function () {
-    for (var k in this.index) {
-      this.index[k].pause();
-    }
+  	for (var k in this.index) {
+  		this.index[k].pause();
+  	}
   };
 
   VideoBackgrounds.prototype.playVideos = function () {
-    for (var k in this.index) {
-      this.index[k].play();
-    }
+  	for (var k in this.index) {
+  		this.index[k].play();
+  	}
   };
 
   VideoBackgrounds.prototype.initYTPlayers = function (callback) {
-    var self = this;
+  	var self = this;
 
-    window.onYouTubeIframeAPIReady = function () {
-      for (var k in self.index) {
+  	window.onYouTubeIframeAPIReady = function () {
+  		for (var k in self.index) {
         if (self.index[k] instanceof YoutubeBackground) {
           self.index[k].initYTPlayer();
         }
-      }
+  		}
 
-      if (callback) {
-        setTimeout(callback, 100);
-      }
-    };
+  		if (callback) {
+  			setTimeout(callback, 100);
+  		}
+  	};
 
-    if (window.hasOwnProperty('YT') && window.YT.loaded) {
-      window.onYouTubeIframeAPIReady();
-    }
+  	if (window.hasOwnProperty('YT') && window.YT.loaded) {
+  		window.onYouTubeIframeAPIReady();
+  	}
   };
 
   if (typeof jQuery == 'function') {
