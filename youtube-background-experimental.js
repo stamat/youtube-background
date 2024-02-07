11bb9344f6654bd123be1aa9cd44dd4715ac8178
Nikola Stamatovic <stamatmail@gmail.com>
2024-02-07 01:03:50 +0100

    Build version 1.1.5

--- youtube-background-experimental.js
+++ youtube-background-experimental.js
@@ -1,4 +1,4 @@
-/* youtube-background v1.1.4 | https://github.com/stamat/youtube-background | MIT License */
+/* youtube-background v1.1.5 | https://github.com/stamat/youtube-background | MIT License */
 (() => {
   // src/lib/controls.js
   var SeekBar = class {
